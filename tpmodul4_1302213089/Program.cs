eb30251a33d2f17d605f7f9d72b8087721475d6d
vikhanmuharram <vikhanmuharram@student.telkomuniversity.ac.id>
2023-03-10 21:42:39 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302213089/Program.cs
+++ tpmodul4_1302213089/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿internal class KodePos
 {
+    enum Kelurahan
+    {
+        Batununggal,
+        Kujangsari,
+        Mengger,
+        Wates,
+        Cijaura,
+        Jatisari,
+        Margasari,
+        Sekejati,
+        Kebonwaru,
+        Maleer,
+        Samoja
+    }
+
+    private static int getKodePos_1302213089(Kelurahan inputKelurahan)
+    {
+        int[] outputKodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        int inputInt = (int)inputKelurahan;
+        return outputKodePos[inputInt];
+    }
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Kelurahan kel = Kelurahan.Mengger;
+        int kodepos = getKodePos_1302213089(kel);
+        Console.WriteLine("Kelurahan " + kel + " memiliki kode pos : " + kodepos);
     }
 }
